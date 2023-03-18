8cec572c6af5b27a63a9a18c423cbb0b07c9d807
sullivannicole <sullivannicole>
2023-03-18 18:03:19 +0000

    Add doc retrieval (via DDG) function for no-title scenario w/ IMDB

--- utils/simplER.py
+++ utils/simplER.py
@@ -13,9 +13,14 @@ import pandas as pd
 from flair.nn import Classifier
 from flair.data import Sentence
 from flair.splitter import SegtokSentenceSplitter
+from bs4 import BeautifulSoup
+import requests
+import re
+import time
+import numpy as np
 
 # -------------------------------
-# Ner functions
+# NER functions
 # -------------------------------
 
 def split_sentences(text):
@@ -55,3 +60,23 @@ def run_NER(df):
         ner_results += sentence_results
 
   return pd.DataFrame(ner_results)
+
+# ---------------------------------
+# Doc retrieval 'graph' function
+# ---------------------------------
+
+def run_search(search_query: str, num_results: int = 5):
+
+  time.sleep(0.25)
+  # Rotate headers
+  headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:84.0) Gecko/20100101 Firefox/84.0'}
+  params = {'q': search_query, 'kl': 'us-en'}
+  
+  page = requests.get('https://html.duckduckgo.com/html', headers = headers, params = params).text
+  soup = BeautifulSoup(page, 'html.parser').find_all("a", class_ = "result__url", href = True, limit = num_results)
+
+  urls = [re.sub('\n', '', x.contents[0]).strip() for x in soup] # get URLs
+  urls_split = [x.split('/')[1:] for x in urls] # Pull out every after first backslash
+  url_titles = [re.sub('-|_', ' ', ' '.join(x)).strip() for x in urls_split] # Concatenate strings into one search term
+
+  return {'search_term': search_query, 'url_title': url_titles}
